8066d8460f0bb48f6dc7d8bfc159de15c215d6d2
Elvis <okaforelvis24@gmail.com>
2021-07-20 03:51:36 -0700

    Amazon Bestseller

--- src/index.js
+++ src/index.js
@@ -1,17 +1,58 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDom from 'react-dom'
+import './index.css'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const books = [
+	{
+	id: 1,
+	img : "https://images-na.ssl-images-amazon.com/images/I/91bgFzUdDcL._AC_UL200_SR200,200_.jpg",
+	title : 'Welcome to Kindergaton',
+	author : 'D.J Steinberg'
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+},
+{
+	id: 2,
+	img: 'https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg',
+	title: 'Love you too',
+	author: 'Amelia Hepworth',
+},
+{
+	id: 3,
+	img: 'https://images-na.ssl-images-amazon.com/images/I/71KilybDOoL._AC_UL200_SR200,200_.jpg',
+	title: 'The Very Hungry Caterpillar',
+	author: 'Eric Carle',
+}
+
+
+]
+
+function BookList(){
+ return (
+		<section className="booklist">
+			{books.map((book)=>{
+				return (
+					<Book key={book.id} book={book}></Book>
+				);
+
+		})}
+			
+		</section>
+ );
+}
+
+const Book = (props) =>{
+	const {img, title, author} = props.book;
+	return (
+		<article className="book">
+			<img src={img} alt="" />
+			<h1>{title}</h1>
+			<h4>{author}</h4>
+			{props.children}
+		
+		</article>
+	);
+}
+
+
+
+ReactDom.render(<BookList/>, document.getElementById('root'));
